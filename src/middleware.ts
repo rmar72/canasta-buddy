c3f7dd4f16fa14f3f3cbea3afdd3fbcda1f758a3
rmar72 <rsolm72@gmail.com>
2025-01-28 22:26:16 -0700

    protecting my-canasta route

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,5 @@
 export { default } from "next-auth/middleware"
 
 export const config = {
-  matcher: ["/", "/about"], // Protect these routes
+  matcher: ["/", "/my-canasta"], // Protect these routes
 };

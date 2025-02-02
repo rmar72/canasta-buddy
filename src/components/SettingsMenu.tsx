57bdf8ba816c47b730ba2e89fb19c9d699b80af0
rmar72 <rsolm72@gmail.com>
2025-02-02 16:51:31 -0700

    code clean up

--- src/components/SettingsMenu.tsx
+++ src/components/SettingsMenu.tsx
@@ -9,10 +9,7 @@ import {
   DropdownMenuShortcut,
   DropdownMenuTrigger,
 } from "@/components/shadcn-ui-components/dropdown-menu"
-import { AiTwotoneEdit } from "react-icons/ai";
-import { AiTwotoneSetting } from "react-icons/ai";
-import { AiTwotoneDelete } from "react-icons/ai";
-import { AiTwotoneFolder } from "react-icons/ai";
+import { AiTwotoneEdit, AiTwotoneSetting, AiTwotoneDelete, AiTwotoneFolder } from "react-icons/ai";
 
 export default function SettingsMenu() {
   return (
